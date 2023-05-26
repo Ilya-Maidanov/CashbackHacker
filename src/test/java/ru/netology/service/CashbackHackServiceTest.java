34624f18813d5e045d95c5394b2f97896008f450
Ilya QA-58 <ilyamaidanov58@yandex.ru>
2023-05-26 19:48:59 +0300

    test commit junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,63 +1,59 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
-
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldPositiveBuy() {
         CashbackHackService service = new CashbackHackService();
 
         int amount = 500;
 
-        org.testng.Assert.assertEquals(service.remain(amount), 500);
+        org.junit.Assert.assertEquals(service.remain(amount), 500);
 
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldBuyZero() {
         CashbackHackService service = new CashbackHackService();
 
         int amount = 0;
 
-        org.testng.Assert.assertEquals(service.remain(amount), 1_000);
+        org.junit.Assert.assertEquals(service.remain(amount), 1_000);
 
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldBuyLimit() {
         CashbackHackService service = new CashbackHackService();
 
         int amount = 1_000;
 
-        org.testng.Assert.assertEquals(service.remain(amount), 0);
+        org.junit.Assert.assertEquals(service.remain(amount), 0);
 
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldBuyMin() {
         CashbackHackService service = new CashbackHackService();
 
         int amount = 1;
 
-        org.testng.Assert.assertEquals(service.remain(amount), 999);
+        org.junit.Assert.assertEquals(service.remain(amount), 999);
 
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldBuyMax() {
         CashbackHackService service = new CashbackHackService();
 
         int amount = 999;
 
-        org.testng.Assert.assertEquals(service.remain(amount), 1);
+        org.junit.Assert.assertEquals(service.remain(amount), 1);
 
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldBuyOverThousand() {
         CashbackHackService service = new CashbackHackService();
 
         int amount = 1_001;
 
-        org.testng.Assert.assertEquals(service.remain(amount), 999);
+        org.junit.Assert.assertEquals(service.remain(amount), 999);
 
     }
 
